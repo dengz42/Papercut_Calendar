ee01cc26089af44724809c5b2adc97a6d3028cf9
Zhongyi Deng <dengz42@mcmaster.ca>
2025-02-01 14:52:56 -0500

    1

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
-function handleClientLoad(){
-    gapi.load('client:auth2', initClient);
-}
-function initClient(){
-    gapi.client.init({
-        apiKey: "AIzaSyBkE-rQQJ-uWpda7Ax3tulbcT18rz3FWqI",
-        clientId: "760407733920-jp8qptmqeog83j3gi2206vft68uips1l.apps.googleusercontent.com",
-        discoveryDocs: "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
-        scope: "https://www.googleapis.com/auth/calendar.events",
-    }).then(function (){
-        // Listen for sign-in state changes
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+function onSignIn(googleResponse) {
+    // The response contains the credential (ID token)
+    const credential = googleResponse.credential;
 
-        // Handle the initial sign-in state
-        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    });
+    // You can send the credential to your backend for verification
+    console.log("Google ID Token: ", credential);
+
+    // Example of getting user info from the token (after verification)
+    const userInfo = parseJwt(credential);
+    console.log('User Info:', userInfo);
 }
-function onSignIn(googleUser){
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
-}
-function handleAuthClick(event){
-    gapi.auth2.getAuthInstance().signIn();
-}
-function handleSignoutClick(event){
-    gapi.auth2.getAuthInstance().signOut();
-}
-function updateSigninStatus(isSignedIn){
-    if(isSignedIn){
-        listUpcomingEvents();
-    } else{
-        // Prompt user to sign in
-    }
+
+// Function to parse JWT token and get user info
+function parseJwt(token) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    
+    return JSON.parse(jsonPayload);
 }
