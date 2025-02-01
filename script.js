7ec7926ad64244e76d402dc02807672a18823d0b
Zhongyi Deng <dengz42@mcmaster.ca>
2025-02-01 12:44:12 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function handleClientLoad(){
+    gapi.load('client:auth2', initClient);
+}
+function initClient(){
+    gapi.client.init({
+        apiKey: "",
+        clientId: "",
+        discoveryDocs: "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+        scope: "https://www.googleapis.com/auth/calendar.events",
+    }).then(function (){
+        // Listen for sign-in state changes
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+        // Handle the initial sign-in state
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    });
+
+}
